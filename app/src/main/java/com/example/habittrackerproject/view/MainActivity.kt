a15dab3c7f99f0e6ba2e9c443e20b49f21cf508c
riyantndjkf <153905200+riyantndjkf@users.noreply.github.com>
2026-04-27 15:58:43 +0700

    Use NavHostFragment in activity_main
    Replace the manual fragment transaction with the AndroidX Navigation component: activity_main.xml now contains a FragmentContainerView (NavHostFragment) with app:navGraph="@navigation/nav_graph" and id @+id/navHost, removing the placeholder TextView. MainActivity.kt no longer performs supportFragmentManager.replace(...) to add DashboardFragment and instead relies on the NavHost; existing FCM token logic is left intact.

--- app/src/main/java/com/example/habittrackerproject/view/MainActivity.kt
+++ app/src/main/java/com/example/habittrackerproject/view/MainActivity.kt
@@ -1,7 +1,6 @@
 package com.example.habittrackerproject.view
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -11,10 +10,6 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        supportFragmentManager.beginTransaction()
-            .replace(R.id.main, DashboardFragment())
-            .commit()
         com.google.firebase.messaging.FirebaseMessaging.getInstance().token.addOnCompleteListener { task ->
             if (!task.isSuccessful) {
                 android.util.Log.w("FCM", "Fetching FCM registration token failed", task.exception)
