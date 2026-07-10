c9ace5aecf25b08494d5d98b19573eac5d5d3227
josephany <josephany.twee@gmail.com>
2026-07-10 16:12:37 +0700

    Update HabitViewModel.kt

--- app/src/main/java/com/example/habittrackerproject/viewmodel/HabitViewModel.kt
+++ app/src/main/java/com/example/habittrackerproject/viewmodel/HabitViewModel.kt
@@ -4,6 +4,7 @@ import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.habittrackerproject.database.AppDatabase
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import com.example.habittrackerproject.model.Habit
@@ -12,26 +13,38 @@ import com.example.habittrackerproject.network.ApiService
 class HabitViewModel(application: Application) : AndroidViewModel(application) {
 
     val habitLD = MutableLiveData<List<Habit>>()
+    val db = AppDatabase.getDatabase(application)
+    val habitDao = db.habitDao()
+
     fun refresh() {
-        ApiService.getHabits(getApplication()) { habits ->
-            habitLD.value = habits
+        viewModelScope.launch(Dispatchers.IO) {
+            val habits = habitDao.getAllHabits()
+            launch(Dispatchers.Main) {
+                habitLD.value = habits
+            }
         }
     }
     fun updateProgress(habit: Habit) {
-        ApiService.updateHabit(getApplication(), habit) {
-            refresh() }
+        viewModelScope.launch(Dispatchers.IO) {
+            habitDao.update(habit)
+            val habits = habitDao.getAllHabits()
+            launch(Dispatchers.Main) {
+                habitLD.value = habits
+            }
+        }
     }
     fun addHabit(habit: Habit) {
-        ApiService.addHabit(getApplication(), habit)
-        refresh()
+        viewModelScope.launch(Dispatchers.IO) {
+            habitDao.insert(habit)
+            val habits = habitDao.getAllHabits()
+            launch(Dispatchers.Main) {
+                habitLD.value = habits
+            }
+        }
     }
-
     fun updateHabitRoom(habit: Habit) {
         viewModelScope.launch(Dispatchers.IO) {
-            // Memanggil fungsi buildDb buatan Anggota 1 dari package util
-            // dan menjalankan @Update dari DOA. (Dummy implementasi agar tidak missing import saat dikerjakan teman)
-            // val db = com.example.habittrackerproject.util.buildDb(getApplication())
-            // db.habitDao().updateHabit(habit)
+            habitDao.update(habit)
         }
     }
 }
